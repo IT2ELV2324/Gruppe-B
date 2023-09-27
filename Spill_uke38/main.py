388cf8478a0930965c18c34cd3a5e1d7dad0a87b
Martin <113992957+martiaro@users.noreply.github.com>
2023-09-27 11:07:27 +0200

    Oppdaterte main.py

--- Spill_uke38/main.py
+++ Spill_uke38/main.py
@@ -1,16 +1,40 @@
 
+import time
 import branch.jeffer as jeffer
 import branch.martin as martin
 import branch.oskar as oskar
 import branch.simen as simen
 import branch.sondre as sondre
 
+SLEEP_TIME = 1
+
+start_liste = ["Jeg ser to mystiske menn komme mot meg",
+               "Jeg sender melding til læreren og skriver at jeg er syk",
+               "Jeg ser en bjørn foran meg",
+               "Jeg merker jeg må pisse",
+               "Jeg går tur meg gruppen min",
+               "Jeg ser et minefelt?"]
+
+
 def introduction_story():
+    print("Det er gym fagdag og du skal på tur med skole gruppen din.")
+    time.sleep(SLEEP_TIME) 
+    print("Du vet ikke helt om du har lyst til dette")
+    time.sleep(SLEEP_TIME)
+    print("Du har nå muligheten til å velge hva du vil gjøre.")
+    time.sleep(SLEEP_TIME)
+    
+def minefelt():
+    print("Gameover")
 
 def start_game():
     introduction_story()
+    print("Skriv et tall mellom ")
+    for i in start_liste:
+        print(i)
+        time.sleep(SLEEP_TIME)
 
-    choice = input()
+    choice = input("Skriv tall mellom 1-6 for å velge sti: ")
 
     if choice == '1':
         jeffer.start()
@@ -22,5 +46,7 @@ def start_game():
         simen.start()
     elif choice == '5':
         sondre.start()
+    else:
+        minefelt()
 
 start_game()
