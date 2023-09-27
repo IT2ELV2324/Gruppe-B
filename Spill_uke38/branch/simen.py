8dfc5b54c97353aae749e64af055b6d02478c4d1
SimenDalsmoKlevdal <sidaa002@osloskolen.no>
2023-09-27 12:50:59 +0200

    update simen.py

--- Spill_uke38/branch/simen.py
+++ Spill_uke38/branch/simen.py
@@ -11,22 +11,35 @@ def alt():
     return valg.lower()
 #stier
     
-DELAY = 2
+DELAY = 1
 
 list = ["Du valgte 4, du begynner å gå ut i skogen vekk fra stien.", "Plutselig hører du en lyd. Kommer den ovenfra?", "Kanskje burde du gå tilbake til de andre"]
-list_2 = ["Du begynner å gå tilbake, lyden synes ikke følge etter deg", "du kommer deg trygt tilbake til stien og får en fet tur"]
-list_3 = ["Du står stille, Du blir blendet av et lys ovenfra", "Plutselig begynner du å sveve oppover", "Når du endelig treffer noe som føles som bakke ser du fortsatt ikke klart", "etter noen sekunder tilpasser synet seg, foran deg står en rar skapning", "Hallo kjære jordboer, vi er fra planeten m29", "Vi gjøre noen eksprimenter på deg, ditt offer vil være for universets beste", "Skal du prøve å rømme?"]
+list_1 = ["Du begynner å gå tilbake, lyden synes ikke følge etter deg", "du kommer deg trygt tilbake til stien og får en fet tur"]
+list_2 = ["Du står stille, Du blir blendet av et lys ovenfra", "Plutselig begynner du å sveve oppover", "Når du endelig treffer noe som føles som bakke ser du fortsatt ikke klart", "etter noen sekunder tilpasser synet seg, foran deg står en rar skapning", "Hallo kjære jordboer, vi er fra planeten m29", "Vi gjøre noen eksprimenter på deg, ditt offer vil være for universets beste", "Skal du prøve å rømme?"]
+list_21 = ["Du dytter karen ned og hopper ut hullet du kom fra", "Du faller i flere sekunder og lander i et tre", "Etter noen sekunder hører du romskipet fly vekk", "du løper tilbake til de andre og forteller hva som skjedde", "Ingen tror på deg"]
 
 
 def sti1():
+    for i in range(len(list_1)):
+        print(list_1[i])
+        time.sleep(DELAY)
+def sti2():
     for i in range(len(list_2)):
         print(list_2[i])
         time.sleep(DELAY)
-def sti2():
-    for i in range(len(list_3)):
-        print(list_3[i])
+    valgte2 = alt()
+    if valgte2 == "ja":
+        sti21()
+    elif valgte2 == "nei":
+        sti22()
+
+def sti21():
+    for i in range(len(list_21)):
+        print(list_21[i])
         time.sleep(DELAY)
-    alt()
+def sti22():
+    print("Du døde")
+    
 def start():
     for i in range(len(list)):
         print(list[i])
@@ -36,4 +49,5 @@ def start():
         sti1()
     elif valgte == "nei":
         sti2()
+
 start()
