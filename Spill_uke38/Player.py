c9a8d89462c2c69d7557691dc2202f800bc23a2d
manysyntaxerrors <sobja004@osloskolen.no>
2023-10-18 10:37:38 +0200

    Update Player.py

--- Spill_uke38/Player.py
+++ Spill_uke38/Player.py
@@ -2,7 +2,6 @@ import random
 
 class Player:
     def __init__(self, name):
+        """ This is a class for defining names of players. This class also randomly selects a xp for each player """
         self.name = name
         self.xp = random.randint(20, 100)
-"""     def mindre_hp(self, hp):
-        self.hp -= hp """
