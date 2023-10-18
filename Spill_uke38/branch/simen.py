29a9e4d68f2aeefc2a5a603ae105e3541a753155
SimenDalsmoKlevdal <sidaa002@osloskolen.no>
2023-10-18 11:12:33 +0200

    dsaflkasfd

--- Spill_uke38/branch/simen.py
+++ Spill_uke38/branch/simen.py
@@ -1,7 +1,14 @@
 #Branch starter her
 import time
+
+#delay for så all teksten ikke kommer ut med en gang  
+DELAY = 1
+
+#skille
 def m():
     print("--------------------")
+
+#alternativ funksjon som returnerer ja eller nei
 def alt():
     rett = False
     while not rett:
@@ -19,16 +26,15 @@ def alt():
                 print("Du må skrive ja eller nei")
         
     return valg.lower()
-#stier
-    
-DELAY = 1
 
+#liste med tekst til de ulike stiene
 list = ["Du valgte 4, du begynner å gå ut i skogen vekk fra stien.", "Plutselig hører du en lyd. Kommer den ovenfra?", "Kanskje burde du gå tilbake til de andre?"]
 list_1 = ["Du begynner å gå tilbake, lyden synes ikke følge etter deg", "du kommer deg trygt tilbake til stien og får en fet tur"]
-list_2 = ["Du står stille, Du blir blendet av et lys ovenfra", "Plutselig begynner du å sveve oppover", "Når du endelig treffer noe som føles som bakke ser du fortsatt ikke klart", "etter noen sekunder tilpasser synet seg, foran deg står en rar skapning", "Hallo kjære jordboer, vi er fra planeten m29", "Vi gjøre noen eksprimenter på deg, ditt offer vil være for universets beste", "Skal du prøve å rømme?"]
+list_2 = ["Du står stille, Du blir blendet av et lys ovenfra", "Plutselig begynner du å sveve oppover", "Når du endelig treffer noe som føles som bakke ser du fortsatt ikke klart", "etter noen sekunder tilpasser synet seg, foran deg står en rar skapning", "Hallo kjære jordboer, vi er fra planeten m29", "Vi skal gjøre noen eksprimenter på deg, ditt offer vil være for universets beste", "Skal du prøve å rømme?"]
 list_21 = ["Du dytter karen ned og hopper ut hullet du kom fra", "Du faller i flere sekunder og lander i et tre", "Etter noen sekunder hører du romskipet fly vekk", "du løper tilbake til de andre og forteller hva som skjedde", "Ingen tror på deg"]
 
-
+#funksjoner for de ulike stiene
+#går bare gjennom den tilhørende listen med delay
 def sti1():
     for i in range(len(list_1)):
         print(list_1[i])
@@ -59,5 +65,4 @@ def start():
         sti1()
     elif valgte == "nei":
         sti2()
-
 start()
